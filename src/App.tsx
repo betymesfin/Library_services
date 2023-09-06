d86b2604e65c9b989ea52a653103823b8259f7a2
betymesfin <98676637+betymesfin@users.noreply.github.com>
2023-09-05 20:18:24 -0400

    navbar component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
 import './App.css';
+import { Navbar } from './Layouts/NavbarAndFooter/Navbarcomponent';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to the fullstack course</h1>
-    </div>
+    <Navbar/>
   );
 }
 
