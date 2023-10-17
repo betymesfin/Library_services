50b0ede701b5f5429905a88236e7373a190f1e94
betymesfin <98676637+betymesfin@users.noreply.github.com>
2023-10-17 11:53:34 -0400

    consuming book from the database to the carousel

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,16 @@ import React from 'react';
 
 import './App.css';
 import { Navbar } from './Layouts/NavbarAndFooter/Navbarcomponent';
-import { ExploreTopBooks } from './Layouts/HomePage/ExploreTopBooks';
-import { Carousel } from './Layouts/HomePage/Carousel';
-import { Heros } from './Layouts/HomePage/Heros';
-import { LibraryServices } from './Layouts/HomePage/LibraryServices';
+import { HomePage } from './Layouts/HomePage/HomePage';
 import { Footer } from './Layouts/NavbarAndFooter/Footer';
 
-function App() {
+export const App = () => {
   return (
     <div>
       <Navbar/>
-      <ExploreTopBooks/>
-      <Carousel/>
-      <Heros/>
-      <LibraryServices/>
+      <HomePage/>
       <Footer/>
     </div>
   );
 }
 
-export default App;
